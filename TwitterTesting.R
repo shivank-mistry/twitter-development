79376ad8a2878e3b5647eec7287f3a8d44069884
Shivank Mistry <shivank.mistry@gmail.com>
2020-04-03 15:45:53 -0600

    added source keys

--- TwitterTesting.R
+++ TwitterTesting.R
@@ -1,2 +1,21 @@
-# install.packages("twitteR")
-library("twitteR")
+# install.packages("rtweet")
+# install.packages("tidytext")
+
+library(rtweet)
+library(tidytext)
+library(ggplot2)
+library(dplyr)
+
+# sourced key files from upper file path, avoided publishing keys
+source("../twitterkeys.R")
+
+# create twitter token
+twitter_token <- create_token(
+  app = app_name,
+  consumer_key = api_key,
+  consumer_secret = api_key_secret,
+  access_token = access_token,
+  access_secret = access_token_Secret)
+
+testing <- search_tweets(q = "#rstats", n = 50)
+
